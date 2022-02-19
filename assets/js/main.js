bc88bf507c36a167a7e4a4f486c2101f6ed9076b
Lance Valle <vallelance8@gmail.com>
2022-02-19 18:00:30 +0800

    add responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,5 +63,35 @@ linkProducts.forEach((link) => {
 })
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUps = document.querySelector('#scroll-up')
+  //when the scroll is higher than 350viewport height, add the show-scroll class to the a tag with the scroll-up class
+  if (this.scrollY >= 350) scrollUps.classList.add('show-scroll')
+  else scrollUps.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
